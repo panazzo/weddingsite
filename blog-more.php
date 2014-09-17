31bd55b3a13e8ea216e1f4b5257cde4df0fbb8c5
panazzo <rpanazzo@dualtec.com.r>
2014-09-16 23:36:25 -0300

    #1

---
+++ blog-more.php
@@ -0,0 +1,276 @@
+<?php
+if ($_REQUEST['page'] == 1)
+{
+/*====================== PAGE 1 ========================*/?>
+<div class="masonry_col_blog w2">
+	<div class="photo_item">
+		<div class="photo_item_preview">
+			<a href="#">
+				<img src="http://placehold.it/553x266" alt="">
+			</a>
+			<div class="photo_item_overlay">
+			<a href="#">
+				<span class="icon_background"></span>
+				<span class="glyphicon glyphicon-link icon_button"></span>
+			</a>
+			</div>
+		</div>
+	</div>
+                        
+	<div class="blog_wrapper">
+		<div class="row">
+			<div class="col-md-12">
+				<h4 class="margin0 padding0"><a href="#">FOREVER LOVE</a></h4>
+				<span class="blog_info">by <a href="#">Admin</a>&nbsp;&nbsp;July 19, 2014&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Wedding</a></span>
+			</div>
+		</div>
+		<div class="row blog_text">
+			<div class="col-md-12">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus, leo at pretium commodo, dui ipsum tristique dolor, in ullamcorper tellus velit quis nisl. In placerat volutpat elit quis dictum. Ut id sem nec mauris lacinia rhoncus. Nunc nec scelerisque arcu.
+				</p>
+			</div>
+		</div>
+	</div>
+</div><!-- END of BLOG ITEM -->
+
+<div class="masonry_col_blog">
+	<div class="photo_item">
+		<div class="photo_item_preview">
+			<a href="#">
+				<img src="http://placehold.it/600x400" alt="">
+			</a>
+			<div class="photo_item_overlay">
+			<a href="#">
+				<span class="icon_background"></span>
+				<span class="glyphicon glyphicon-link icon_button"></span>
+			</a>
+			</div>
+		</div>
+	</div>
+                        
+	<div class="blog_wrapper">
+		<div class="row">
+			<div class="col-md-12">
+				<h4 class="margin0 padding0"><a href="#">HAPPY DAY</a></h4>
+				<span class="blog_info">by <a href="#">Admin</a>&nbsp;&nbsp;July 12, 2014&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Wedding</a></span>
+			</div>
+		</div>
+		<div class="row blog_text">
+			<div class="col-md-12">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus, leo at pretium commodo, dui ipsum tristique dolor, in ullamcorper tellus velit quis nisl.
+				</p>
+			</div>
+		</div>
+	</div>
+</div><!-- END of BLOG ITEM -->
+
+<div class="masonry_col_blog">
+	<div class="photo_item">
+		<div class="photo_item_preview">
+			<a href="#">
+				<img src="http://placehold.it/600x438" alt="">
+			</a>
+			<div class="photo_item_overlay">
+			<a href="#">
+				<span class="icon_background"></span>
+				<span class="glyphicon glyphicon-link icon_button"></span>
+			</a>
+			</div>
+		</div>
+	</div>
+                        
+	<div class="blog_wrapper">
+		<div class="row">
+			<div class="col-md-12">
+				<h4 class="margin0 padding0"><a href="#">LOVELY FAMILY</a></h4>
+				<span class="blog_info">by <a href="#">Admin</a>&nbsp;&nbsp;July 03, 2014&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Wedding</a></span>
+			</div>
+		</div>
+		<div class="row blog_text">
+			<div class="col-md-12">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus, leo at pretium commodo, dui ipsum tristique dolor, in ullamcorper tellus velit quis nisl.
+				</p>
+			</div>
+		</div>
+	</div>
+</div><!-- END of BLOG ITEM -->
+
+<div class="masonry_col_blog">
+	
+	<div class="photo_item">
+		<div class="photo_item_preview">
+			<a href="#">
+				<img src="http://placehold.it/355x355" alt="">
+			</a>
+			<div class="photo_item_overlay">
+			<a href="#">
+				<span class="icon_background"></span>
+				<span class="glyphicon glyphicon-link icon_button"></span>
+			</a>
+			</div>
+		</div>
+	</div>
+</div><!-- END of BLOG ITEM -->
+
+<div class="masonry_col_blog">
+	
+	<div class="photo_item">
+		<div class="photo_item_preview">
+			<a href="#">
+				<img src="http://placehold.it/355x355" alt="">
+			</a>
+			<div class="photo_item_overlay">
+			<a href="#">
+				<span class="icon_background"></span>
+				<span class="glyphicon glyphicon-link icon_button"></span>
+			</a>
+			</div>
+		</div>
+	</div>
+</div><!-- END of BLOG ITEM -->
+<?php
+}
+elseif ($_REQUEST['page'] == 2)
+{
+/*====================== PAGE 2 ========================*/?>
+<div class="masonry_col_blog w2">
+	<div class="photo_item">
+		<div class="photo_item_preview">
+			<a href="#">
+				<img src="http://placehold.it/553x266" alt="">
+			</a>
+			<div class="photo_item_overlay">
+			<a href="#">
+				<span class="icon_background"></span>
+				<span class="glyphicon glyphicon-link icon_button"></span>
+			</a>
+			</div>
+		</div>
+	</div>
+                        
+	<div class="blog_wrapper">
+		<div class="row">
+			<div class="col-md-12">
+				<h4 class="margin0 padding0"><a href="#">ANOTHER LOVES</a></h4>
+				<span class="blog_info">by <a href="#">Admin</a>&nbsp;&nbsp;June 26, 2014&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Wedding</a></span>
+			</div>
+		</div>
+		<div class="row blog_text">
+			<div class="col-md-12">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus, leo at pretium commodo, dui ipsum tristique dolor, in ullamcorper tellus velit quis nisl. In placerat volutpat elit quis dictum. Ut id sem nec mauris lacinia rhoncus. Nunc nec scelerisque arcu.
+				</p>
+			</div>
+		</div>
+	</div>
+</div><!-- END of BLOG ITEM -->
+
+<div class="masonry_col_blog">
+	<div class="photo_item">
+		<div class="photo_item_preview">
+			<a href="#">
+				<img src="http://placehold.it/600x400" alt="">
+			</a>
+			<div class="photo_item_overlay">
+			<a href="#">
+				<span class="icon_background"></span>
+				<span class="glyphicon glyphicon-link icon_button"></span>
+			</a>
+			</div>
+		</div>
+	</div>
+                        
+	<div class="blog_wrapper">
+		<div class="row">
+			<div class="col-md-12">
+				<h4 class="margin0 padding0"><a href="#">ANOTHER HAPPY DAY</a></h4>
+				<span class="blog_info">by <a href="#">Admin</a>&nbsp;&nbsp;June 12, 2014&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Wedding</a></span>
+			</div>
+		</div>
+		<div class="row blog_text">
+			<div class="col-md-12">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus, leo at pretium commodo, dui ipsum tristique dolor, in ullamcorper tellus velit quis nisl.
+				</p>
+			</div>
+		</div>
+	</div>
+</div><!-- END of BLOG ITEM -->
+
+<div class="masonry_col_blog">
+	<div class="photo_item">
+		<div class="photo_item_preview">
+			<a href="#">
+				<img src="http://placehold.it/600x438" alt="">
+			</a>
+			<div class="photo_item_overlay">
+			<a href="#">
+				<span class="icon_background"></span>
+				<span class="glyphicon glyphicon-link icon_button"></span>
+			</a>
+			</div>
+		</div>
+	</div>
+                        
+	<div class="blog_wrapper">
+		<div class="row">
+			<div class="col-md-12">
+				<h4 class="margin0 padding0"><a href="#">ANOTHER LOVELY FAMILY</a></h4>
+				<span class="blog_info">by <a href="#">Admin</a>&nbsp;&nbsp;June 12, 2014&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Wedding</a></span>
+			</div>
+		</div>
+		<div class="row blog_text">
+			<div class="col-md-12">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus, leo at pretium commodo, dui ipsum tristique dolor, in ullamcorper tellus velit quis nisl.
+				</p>
+			</div>
+		</div>
+	</div>
+</div><!-- END of BLOG ITEM -->
+
+<div class="masonry_col_blog">
+	
+	<div class="photo_item">
+		<div class="photo_item_preview">
+			<a href="#">
+				<img src="http://placehold.it/355x355" alt="">
+			</a>
+			<div class="photo_item_overlay">
+			<a href="#">
+				<span class="icon_background"></span>
+				<span class="glyphicon glyphicon-link icon_button"></span>
+			</a>
+			</div>
+		</div>
+	</div>
+</div><!-- END of BLOG ITEM -->
+
+<div class="masonry_col_blog">
+	
+	<div class="photo_item">
+		<div class="photo_item_preview">
+			<a href="#">
+				<img src="http://placehold.it/355x355" alt="">
+			</a>
+			<div class="photo_item_overlay">
+			<a href="#">
+				<span class="icon_background"></span>
+				<span class="glyphicon glyphicon-link icon_button"></span>
+			</a>
+			</div>
+		</div>
+	</div>
+</div><!-- END of BLOG ITEM -->
+<?php
+}
+else
+{
+/*====================== NO MORE ITEM ========================*/?>
+<?php
+	$more = "empty";
+	die($more);
+}?>
